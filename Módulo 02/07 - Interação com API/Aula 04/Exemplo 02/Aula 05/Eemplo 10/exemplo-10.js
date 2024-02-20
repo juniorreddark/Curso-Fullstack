9b46494378683b093ff7a76d4b6ec2db18185632
dark <juniorantoniojunior584@gmail.com>
2024-02-20 11:31:45 -0300

    dark

--- Módulo 02/07 - Interação com API/Aula 04/Exemplo 02/Aula 05/Eemplo 10/exemplo-10.js
+++ Módulo 02/07 - Interação com API/Aula 04/Exemplo 02/Aula 05/Eemplo 10/exemplo-10.js
@@ -4,8 +4,33 @@ async function buscar(){
     //console.log(procura)
     let produtos = await procura.json()
     let listaDiv= document.getElementById("lista-card")
-    for (let x in produtos){
-        listaDiv.innerHTML +=`<h1>${produtos[x].nome}</h1`
+    for (let produto of produtos){
+        listaDiv.innerHTML +=`
+
+            <div class="card">
+                <img src="${produto.img}"  width="250" height="auto">                
+                <h3 class="titulo">
+                ${produto.nome}
+                </h3>
+                <p>
+                ${produto.descricao}
+                </p>
+                <div class="valores">
+                    <span class="valorSemDesconto">
+       
+                        R$${(produto.valorComDesconto).toFixed(2).replace(".",",")}
+                    </span>
+
+                    <span class="valorComDesconto">
+                        R$${(produto.valorSemDesconto).toFixed(2).replace(".",",")}
+                    </span>
+                </div>
+
+               
+
+            </div>
+        `
+            
 
     }
 
