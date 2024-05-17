4e33c717be9e11f2eb4877432c37f05eb50f7c06
juniorreddark <juniorantoniojunior584@gmail.com>
2024-05-17 11:21:43 -0300

    dark

---
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/Projeto03/apps/file/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Arquivos(models.Model):
+    nome = models.CharField(max_length=50)
+    foto = models.ImageField(upload_to="foto_perfil/")
+    def __str__(self):
+        return self.nome
+    
