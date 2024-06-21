1f902c5183e38521b693670f5e1580a3c4aea879
juniorreddark <juniorantoniojunior584@gmail.com>
2024-06-21 11:20:28 -0300

    dark red

--- Treinamento/Americanas/apps/post/models.py
+++ Treinamento/Americanas/apps/post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=50)
+    valor_produto = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.nome_produto
+    
--- Treinamento/Americanas/apps/post/views.py
+++ Treinamento/Americanas/apps/post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def VerProdutos(request):
+    produtos_lista = Produto.objects.all()
+    return render(request, "index.html", {"produtos":produtos_lista})
+
+def DetalhesProduto(request, id_produto):
+    busca = Produto.objects.get(id=id_produto)
+    return render(request, "detalhes_produto.html", {"produto":busca})
